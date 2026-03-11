558e0483e2abc6ea4b42703a333eede35e32f68a
feifxi <feoy1234@gmail.com>
2026-03-11 20:36:31 +0700

    add gitignore

--- twitter-next-web/src/app/providers.tsx
+++ twitter-next-web/src/app/providers.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { SignInModal } from '@/components/auth/SignInModal';
-import { SignUpModal } from '@/components/auth/SignUpModal';
-import { AuthInitializer } from '@/components/auth/AuthInitializer';
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { SignInModal } from "@/components/auth/SignInModal";
+import { SignUpModal } from "@/components/auth/SignUpModal";
+import { AuthInitializer } from "@/components/auth/AuthInitializer";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
@@ -13,16 +13,23 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
       <QueryClientProvider client={queryClient}>
-      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-        <AuthInitializer>
-          {children}
-          <SignInModal />
-          <SignUpModal />
-        </AuthInitializer>
-      </GoogleOAuthProvider>
-    </QueryClientProvider>
+        <GoogleOAuthProvider
+          clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}
+        >
+          <AuthInitializer>
+            {children}
+            <SignInModal />
+            <SignUpModal />
+          </AuthInitializer>
+        </GoogleOAuthProvider>
+      </QueryClientProvider>
     </NextThemesProvider>
   );
 }
